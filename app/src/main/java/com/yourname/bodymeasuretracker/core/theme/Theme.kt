834e249c014f894329889bd2f7fa51bdddfeacba
maksim <guz42@bk.ru>
2025-11-15 14:27:56 +0700

    feat_v1

---
+++ app/src/main/java/com/yourname/bodymeasuretracker/core/theme/Theme.kt
@@ -0,0 +1,13 @@
+package com.yourname.bodymeasuretracker.core.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BodyMeasureTrackerTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
+}
